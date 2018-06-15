9be8c6e1aaf18226ca0e4a99d3ea70c1291c6b12
LK <sfandy@126.com>
2018-06-15 11:06:50 +0800

    Initial Commit

---
+++ LKLocalizedDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LKLocalizedDemo
+//
+//  Created by CK on 2018/6/15.
+//  Copyright © 2018年 LK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LKLocalizedDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LKLocalizedDemo
+//
+//  Created by CK on 2018/6/15.
+//  Copyright © 2018年 LK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
